3110a3453b613db5ccfe3fd0b11d8ce20dfb9d30
Cichykacik <cichykacik406@gmail.com>
2017-03-15 22:46:39 +0100

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,14 @@
 
 var myImage = document.querySelector('img');
 
-myImage.onclick = function() {
+/*myImage.onclick = function() {
 	var mySrc = myImage.getAttribute('src');
-	if(mySrc === 'images/chlon.jpg') {
-      myImage.setAttribute ('src','images/chlon2.jpg');
+	if(mySrc === 'images/yee.jpg') {
+      myImage.setAttribute ('src','images/yee2.jpg');
 	} else {
-	  myImage.setAttribute ('src','images/chlon.jpg');
+	  myImage.setAttribute ('src','images/yee.jpg');
 	}
-}
+}*/
 
 
 var myButton = document.querySelector('button');
@@ -18,16 +18,27 @@ var myHeading = document.querySelector('h3');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.innerHTML = 'mgr inż. Kazimierz ' + myName;
+  myHeading.innerHTML =  'Wszystko co ' + myName + ' chciałby/chciałaby wiedzieć o Yee';
 }
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'mgr inż. Kazimierz ' + storedName;
+  myHeading.innerHTML = 'Wszystko co ' + storedName + ' chciałby/chciałaby wiedzieć o Yee';
 }
 
 myButton.onclick = function() {
   setUserName();
+}
+
+var myYee = document.getElementById('je'); 
+
+myYee.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/yee.jpg') {
+      myImage.setAttribute ('src','images/yee2.jpg');
+	} else {
+	  myImage.setAttribute ('src','images/yee.jpg');
+	}
 }
